586a205aeff4b8ae681a51c71e09c10772dcb4c8
afdlolnur <dddlllooolll@gmail.com>
2020-10-30 04:20:10 +0700

    model

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'price', 'rate', 'types',
+        'picturePath'
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    //karena picturePath nggak pake underscore ini buat ngakalin 
+    public function toArray()
+    {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return $toArray;
+    }
+
+    //fungsi unutk ngubah nama field
+    public function getPicturePathAttribute()
+    {
+        return url('') . Storage::url($this->attributes['picturePath']);
+    }
+
 }
