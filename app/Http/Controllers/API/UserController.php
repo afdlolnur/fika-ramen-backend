c3444717b5d25a4b3278e80ca588ac889af318c7
afdlolnur <dddlllooolll@gmail.com>
2020-10-31 17:28:05 +0700

    api uploadPhoto

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -104,7 +104,6 @@ class UserController extends Controller
         return ResponseFormatter::success($request->user(),'Data profile berhasil diambil');
     }
     
-
     public function updateProfile(Request $request)
     {
         $data = $request->all();
@@ -113,5 +112,29 @@ class UserController extends Controller
         $user->update($data);
 
         return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048',
+        ]);
+
+        //jika validator gagal
+        if ($validator->fails()) {
+            return ResponseFormatter::error(['error'=>$validator->errors()], 'Update Photo Fails', 401);
+        }
+
+        if ($request->file('file')) {
+
+            $file = $request->file->store('assets/user', 'public');
+
+            //simpan file(url) ke database
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file],'File successfully uploaded');
+        }
     }
 }
